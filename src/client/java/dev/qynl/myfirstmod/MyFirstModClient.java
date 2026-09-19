b6f4280139c71208e4148ed703831d1a4224bfce
Qynl <qynlden@tutamail.com>
2026-09-19 18:37:58 +0200

    Create initial MyFirstMod project

---
+++ src/client/java/dev/qynl/myfirstmod/MyFirstModClient.java
@@ -0,0 +1,9 @@
+package dev.qynl.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
