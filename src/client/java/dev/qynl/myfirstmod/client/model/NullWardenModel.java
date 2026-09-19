3fea20b7cf310caaef5a5961f9211ff5ac7fa83f
Qynl <qynlden@tutamail.com>
2026-09-19 20:15:37 +0200

    Fix Null Warden model imports

--- src/client/java/dev/qynl/myfirstmod/client/model/NullWardenModel.java
+++ src/client/java/dev/qynl/myfirstmod/client/model/NullWardenModel.java
@@ -8,6 +8,7 @@ import net.minecraft.client.model.ModelPartBuilder;
 import net.minecraft.client.model.ModelPartData;
 import net.minecraft.client.model.ModelTransform;
 import net.minecraft.client.model.TexturedModelData;
+import net.minecraft.client.render.entity.model.SinglePartEntityModel;
 import net.minecraft.util.math.MathHelper;
 
 public class NullWardenModel extends SinglePartEntityModel<NullWardenEntity> {
