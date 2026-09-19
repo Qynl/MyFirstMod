0ddb0a880affa25701c4363c99b3639008870d44
Qynl <qynlden@tutamail.com>
2026-09-19 18:52:58 +0200

    Add the Nullblade reward

--- src/main/java/dev/qynl/myfirstmod/item/ModItems.java
+++ src/main/java/dev/qynl/myfirstmod/item/ModItems.java
@@ -2,6 +2,8 @@ package dev.qynl.myfirstmod.item;
 
 import dev.qynl.myfirstmod.MyFirstMod;
 import net.minecraft.item.Item;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -11,6 +13,12 @@ public final class ModItems {
             Registries.ITEM,
             Identifier.of(MyFirstMod.MOD_ID, "null_relic"),
             new Item(new Item.Settings().maxCount(1))
+    );
+
+    public static final Item NULLBLADE = Registry.register(
+            Registries.ITEM,
+            Identifier.of(MyFirstMod.MOD_ID, "nullblade"),
+            new NullbladeItem(new Item.Settings().maxCount(1).maxDamage(2031))
     );
 
     public static void register() {}
