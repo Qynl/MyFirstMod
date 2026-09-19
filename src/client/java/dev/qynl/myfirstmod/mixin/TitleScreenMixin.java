10dfa3c32504d21f31520c01aa661ba0ca7a110f
Qynl <qynlden@tutamail.com>
2026-09-19 18:38:21 +0200

    Fix title screen mixin

--- src/client/java/dev/qynl/myfirstmod/mixin/TitleScreenMixin.java
+++ src/client/java/dev/qynl/myfirstmod/mixin/TitleScreenMixin.java
@@ -5,6 +5,9 @@ import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
@@ -12,13 +15,8 @@ public class TitleScreenMixin {
     @Shadow
     private SplashRenderer splash;
 
-    @Shadow
-    private boolean fading;
-
-    static {
-    }
-
-    private void myFirstMod$clean() {
+    @Inject(method = "init", at = @At("TAIL"))
+    private void myFirstMod$cleanMenu(CallbackInfo info) {
         splash = null;
     }
 }
