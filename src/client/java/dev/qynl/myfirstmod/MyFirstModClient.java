ab098ff38101ad29f6c4c71e2dd416e15c8f92d4
Qynl <qynlden@tutamail.com>
2026-09-19 18:46:29 +0200

    Render the Void Gate as a translucent portal

--- src/client/java/dev/qynl/myfirstmod/MyFirstModClient.java
+++ src/client/java/dev/qynl/myfirstmod/MyFirstModClient.java
@@ -1,9 +1,13 @@
 package dev.qynl.myfirstmod;
 
+import dev.qynl.myfirstmod.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class MyFirstModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.VOID_PORTAL, RenderLayer.getTranslucent());
     }
 }
